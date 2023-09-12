8a139c52ba9f79e27c9fe6a900b13afc7d33b01f
Kiran <kirankumardn74@gmail.com>
2023-09-12 23:10:52 +0530

    added pages

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.write("welcome to my home page");
-  res.end();
+  if (req.url === "/home") {
+    res.end("welcome to our home page");
+  }
+
+  if (req.url === "/about") {
+    res.end("here is our short history");
+  }
+  res.end(`<h1>Oops!!</h1>
+  <p>we cont seem tofind the page you are looking for</p>
+  <a href='/home'>back home</a>
+  `);
 });
 console.log("server is running in local host 5000");
 server.listen(5000);
