2359cfc69dd319b9be7264f6a13f28047bcb28a3
Kiran <kirankumardn74@gmail.com>
2023-09-13 19:01:50 +0530

    blocking code added in app js file

--- app.js
+++ app.js
@@ -8,13 +8,23 @@ const server = http.createServer((req, res) => {
   if (req.url === "/about") {
     res.end("here is our short history");
   }
+  //blocking code
+  if (req.url === "/blockcode") {
+    for (let i = 0; i < 1000; i++) {
+      for (let j = 0; j < 1000; j++) {
+        console.log(`${i} ${j}`);
+      }
+    }
+  }
   res.end(`<h1>Oops!!</h1>
   <p>we cont seem tofind the page you are looking for</p>
   <a href='/home'>back home</a>
   `);
 });
-console.log("server is running in local host 5000");
-server.listen(5000);
+
+server.listen(5000, () => {
+  console.log("server listening on port:5000...");
+});
 
 const lodash = require("lodash");
 
@@ -23,3 +33,5 @@ const items = [1, [2, [3, [4]]]];
 const newItems = lodash.flattenDeep(items);
 
 console.log("new item is", newItems);
+
+console.log("Hello People ");
