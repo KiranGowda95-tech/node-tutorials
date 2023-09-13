7955cd62764a26e9f0d441c2a191903aa23f4057
Kiran <kirankumardn74@gmail.com>
2023-09-13 07:08:31 +0530

    package file updated

--- app.js
+++ app.js
@@ -15,3 +15,11 @@ const server = http.createServer((req, res) => {
 });
 console.log("server is running in local host 5000");
 server.listen(5000);
+
+const lodash = require("lodash");
+
+const items = [1, [2, [3, [4]]]];
+
+const newItems = lodash.flattenDeep(items);
+
+console.log("new item is", newItems);
