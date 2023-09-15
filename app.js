77154c16d14fda9f5f25b6ddae2b5a87fdb84baa
Kiran <kirankumardn74@gmail.com>
2023-09-15 15:23:12 +0530

    simple project of navbar application

--- app.js
+++ app.js
@@ -110,9 +110,12 @@
 // });
 
 const http = require("http");
-const { readFileSync } = reqire("fs");
+const { readFileSync } = require("fs");
 //get all files
-const homePage = readFileSync("./index.html");
+const homePage = readFileSync("./navbar-app/index.html");
+const homeStyle = readFileSync("./navbar-app/styles.css");
+const homeImage = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
 const server = http.createServer((req, res) => {
   const url = req.url;
   //Home page
@@ -125,6 +128,24 @@ const server = http.createServer((req, res) => {
   else if (url === "/about") {
     res.writeHead(200, { "content-type": "text/html" });
     res.write("<h1>about page</h1>");
+    res.end();
+  }
+  //styles
+  else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyle);
+    console.log("entered to style.css");
+
+    res.end();
+  }
+  //images/logo
+  else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(homeImage);
+    res.end();
+  } else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(homeLogic);
     res.end();
   }
   //404
